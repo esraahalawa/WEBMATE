f2f7226b353defaa7110edabd569d6dc8f31eccc
esraahalawa <54468353+esraahalawa@users.noreply.github.com>
2019-08-24 11:12:15 +0200

    Update file.js

--- js/file.js
+++ js/file.js
@@ -1,47 +1,12 @@
+
+/* global $, jquery, alert */ 
+
 $(document).ready(function (){
     "use strict";
-    $('.checkout').click(function() {
-        
-      alert("The Shopping Detail Is = 'Products' And The total Cost = '' .");
-    });
-    
-    $('.send').click(function() {
-      alert("The Message Sent");
-    });
-    
-    $("input[type='submit'").click(function(){
-        alert("Search Loading .....");
-    });
-    
-    $(".adding1").click(function() {
-        $('.glyphicon-shopping-car').text(1);
-    });
-    
-    $(".remove1").click(function() {
-        $(".div1").remove();
-    }) ;
-    $(".remove2").click(function() {
-        $(".div2").remove();
-    }) ;
-    
-     $(".remove3").click(function() {
-        $(".div3").remove();
-    }) ;
-    $(".remove4").click(function() {
-        $(".div4").remove();
-    }) ;
-    
-     $(".remove5").click(function() {
-        $(".div5").remove();
-    }) ;
-    $(".remove6").click(function() {
-        $(".div6").remove();
-    }) ;
-        
     $("html").niceScroll();
     $('.carousel').carousel(
     {
-        interval:3000
+        interval:5000
     });
     $(".gear-check").click(function () {
          $(".color-option").fadeToggle();                   
